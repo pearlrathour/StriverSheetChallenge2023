d7377b8b469e280b55120202235f931435c91ad2
Pearl Rathour <99817664+pearlrathour@users.noreply.github.com>
2023-06-20 00:02:50 +0530

    Time: 0 ms (100%), Space: 7.5 MB (13.39%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
+    void helper(int n, vector<vector<int>>&res,vector<int>&r){
+        if(n==1){
+            res.push_back(r);
+            return;
+        }
+        helper(n-1, res, r);
+        vector<int>b(n);
+        b[0]=1;
+        for(int i=1; i<n-1; i++){
+            b[i]=res[n-2][i] + res[n-2][i-1];
+        }
+        b[n-1]=1;
+        res.push_back(b);
+    }
     vector<vector<int>> generate(int n) {
         vector<vector<int>>res;
-        for(int i=0; i<n; i++){
-            vector<int>r(i+1,1);
-            for(int j=1; j<i; j++){
-                r[j]= res[i-1][j-1] + res[i-1][j];
-            }
-            res.push_back(r);
-        }
+        vector<int>r={1};
+        helper(n, res, r);
         return res;
     }
 };
