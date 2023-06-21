6d42570c49e6a91b3b3ff24ff3c017a071b25298
Pearl Rathour <99817664+pearlrathour@users.noreply.github.com>
2023-06-21 14:33:41 +0530

    Time: 0 ms (100%), Space: 9.1 MB (16.19%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,11 +6,10 @@ public:
         int k = m + n - 1;
         
         while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
+            if (i >= 0 && nums1[i] > nums2[j])
                 nums1[k--] = nums1[i--];
-            } else {
+            else
                 nums1[k--] = nums2[j--];
-            }
         }
     }
 };
