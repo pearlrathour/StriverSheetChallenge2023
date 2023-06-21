f2226ca72b36a4cbbef9bba598fb230fb9488a80
Pearl Rathour <99817664+pearlrathour@users.noreply.github.com>
2023-06-21 13:15:43 +0530

    Time: 44 ms (54.74%), Space: 19.5 MB (33.02%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intrv) {
+        int n= intrv.size();
+        if(n==1)
+            return intrv;
+
+        sort(intrv.begin(), intrv.end());
+        vector<vector<int>>ans;
+        int l=intrv[0][0], r=intrv[0][1];
+
+        for(int i=1; i<n; i++){
+            
+            if(intrv[i][0] >= l && intrv[i][0] <= r){
+                if(intrv[i][1] >= r){
+                    r=intrv[i][1];
+                }
+            }
+            // else if(intrv[i][0] < l){
+            //     l=intrv[i][0];
+            //     if(intrv[i][1] >= r){
+            //         r=intrv[i][1];
+            //     }
+            // }
+            else{
+                vector<int>a={l,r};
+                ans.push_back(a);
+                l=intrv[i][0];
+                r=intrv[i][1];
+            }
+        }
+        vector<int>a={l,r};
+        ans.push_back(a);
+        return ans;
+    }
+};
