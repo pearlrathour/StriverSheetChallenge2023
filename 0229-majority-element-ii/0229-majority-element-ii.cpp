e1f42d78c7d64c57c826ef49fdde569218fed975
Pearl Rathour <99817664+pearlrathour@users.noreply.github.com>
2023-08-01 18:20:05 +0530

    Time: 12 ms (78.26%), Space: 15.9 MB (63.37%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -7,14 +7,16 @@ public:
 
         vector<int>res;
         unordered_map<int,int>ump;
-        for(int i=0; i<l; i++)
+        int n=(l/3)+1;
+        for(int i=0; i<l; i++){
             ump[nums[i]]++;
 
-        for(auto x : ump){
-            if(x.second > (l/3))
-                res.push_back(x.first);
-        }
+            if(ump[nums[i]] == n)
+                res.push_back(nums[i]);
 
+            if(res.size()==2)
+                return res;
+        }
         return res;
     }
 };
