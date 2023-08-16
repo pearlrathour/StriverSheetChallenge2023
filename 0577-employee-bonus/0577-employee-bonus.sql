d18cd75961717f108865771a25837002ced9618b
Pearl <pearl.rathour33@gmail.com>
2023-08-16 21:09:51 +0530

    Added Day 03 Problems

--- 0577-employee-bonus/0577-employee-bonus.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT E.NAME, B.BONUS
-FROM Employee E
-LEFT JOIN Bonus B
-ON E.empId=B.empId
-WHERE B.bonus<1000 OR B.bonus IS NULL;
--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++
@@ -1,7 +0,0 @@
-# Write your MySQL query statement below
-Select a1.machine_id, round(avg(a2.timestamp-a1.timestamp),3) as  processing_time 
-from Activity a1, Activity a2
-where a1.machine_id=a2.machine_id
-and a1.process_id=a2.process_id
-and a1.activity_type='start' and a2.activity_type='end'
-group by a1.machine_id;
